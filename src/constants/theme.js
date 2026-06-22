510d10d3aae65252527c88ee84b631be1db21aea
Pedro Sotelo <pedripjs@gmail.com>
2026-06-22 13:10:52 -0600

    Create theme.js & define basic coloring
    This prolly will be modified moving forward

---
+++ src/constants/theme.js
@@ -0,0 +1,70 @@
+// Style definition for the app
+
+export const Colors = {
+    // Base
+    ink: '#0F0E0C',
+    paper: '#F7F4EE',
+    warmMid: '#E8E2D5',
+    white: '#FFFFFF',
+    muted: '#8C8880',
+    background: '#DEDAD3',
+
+    // Accents
+    sage: '#3D5A4C',
+    sageLt: '#EBF2EE',
+
+    // Expenses
+    red: '#B94040',
+    redLt: '#FAEAEA',
+
+    // Withdraws
+    amber: '#C9822A',
+    amberLt: '#FDF0E0',
+
+    // Savings
+    purple: '#6B5B9E',
+    purpleLt: '#EEE8F7'
+};
+
+export const FontSize = {
+    xs: 10,
+    sm: 12,
+    md: 14,
+    lg: 16,
+    xl: 20,
+    xxl: 28,
+    hero: 44,
+};
+
+export const Radius = {
+    sm: 12,
+    md: 18,
+    lg: 28,
+    xl: 36,
+    full: 999,
+};
+
+export const Spacing = {
+    xs: 4,
+    sm: 8,
+    md: 16,
+    lg: 24,
+    xl: 32,
+};
+
+export const Shadow = {
+    card: {
+        shadowColor: '#0F0E0C',
+        shadowOffset: { width: 0, height: 2},
+        shadowOpacity: 0.08,
+        shadowRadius: 8,
+        elevation: 3,
+    },
+    float: {
+        shadowColor: '#0F0E0C',
+        shadowOffset: { width: 0, height: 8},
+        shadowOpacity: 0.14,
+        shadowRadius: 20,
+        elevation: 8,
+    },
+};
