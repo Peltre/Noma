3c10e60af007ee4c89442ffb8625b5e618cca4ec
Pedro Sotelo <pedripjs@gmail.com>
2026-06-23 11:40:09 -0600

    Update to use AsyncStorage instead of MMKV which can cause issues down the line

--- src/store/storage.js
+++ src/store/storage.js
@@ -1,21 +1,19 @@
 // The heart of this app; where all data lives happily
 
-import { MMKV } from 'react-native-mmkv'; // Wrapper for data persistance
-
-export const storage = new MMKV({ id: 'noma-storage' });
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Helper functions to help store, retrieve & remove data
 // (JSON) objs
-export const saveData = (key, value) => {
-    storage.set(key, JSON.stringify(value))
+export const saveData = async (key, value) => {
+  await AsyncStorage.setItem(key, JSON.stringify(value));
 };
 
-export const loadData = (key) => {
-    const value = storage.getString(key);
-    if (!value) return null;
-    return JSON.parse(value);
+export const loadData = async (key) => {
+  const value = await AsyncStorage.getItem(key);
+  if (!value) return null;
+  return JSON.parse(value);
 };
 
-export const removeData = (key) => {
-    storage.delete(key);
+export const removeData = async (key) => {
+  await AsyncStorage.removeItem(key);
 };
